e9e4d0159710b1dbbced8838056e9b7bcb304162
Guillaume240192 <146423617+Guillaume240192@users.noreply.github.com>
2024-02-25 00:10:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,12 @@ axios(url)
 }).catch(err => console.log(err))
 
 */
-
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 
