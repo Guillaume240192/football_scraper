ffe8807a2ea4323e8b5c21772186635270f526a0
Guillaume240192 <146423617+Guillaume240192@users.noreply.github.com>
2024-02-25 15:20:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,35 +9,29 @@ const app = express();
 const url = 'https://www.ligue1.fr/classement';
 
 
-
-// usefull to allow request from all the websites (*)
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
-
-
 app.get('/', (req,res)=>{
     let clubNameTab = [];
- 
+    
     axios(url)
     .then(response => {
         html = response.data;
         //console.log(html);
         const $ = cheerio.load(html);
+        clubNameTab = [];
         $('span.GeneralStats-clubName.mobile-item',html).each(function() {
             const clubName = $(this).text();
             clubNameTab.push(clubName);
             //console.log(clubName);
+
         })
-        //console.log(clubNameTab);
+        res.send(clubNameTab);
+        res.end();
+        console.log(clubNameTab);
         
     }).catch(err => console.log(err))
-    res.json(clubNameTab);
+
      
 })
 
 app.listen(port , () => console.log(`Server running on PORT ${port}`));
+
